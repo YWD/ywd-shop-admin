664b7aaad642bcce5762558e3465d9b6c2f5f31f
杨文冬 <1347011912@qq.com>
2022-04-28 22:25:39 +0800

    feat: login布局实现

--- src/router/router-index.ts
+++ src/router/router-index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import HomeIndex from '../views/home/HomeIndex.vue'
 import LoginIndex from '../views/login/LoginIndex.vue'
 import AppLayout from '@/layout/AppLayout.vue'
+import HtmlTest from '@/views/test/HtmlTest.vue'
 import productRoute from './modules/product'
 import nprogress from 'nprogress'
 import 'nprogress/nprogress.css'
@@ -23,6 +24,11 @@ const routes: RouteRecordRaw[] = [
     path: '/login',
     name: 'login',
     component: LoginIndex
+  },
+  {
+    path: '/test',
+    name: 'test',
+    component: HtmlTest
   }
 ]
 
--- src/views/login/LoginIndex.vue
+++ src/views/login/LoginIndex.vue
@@ -1,11 +1,73 @@
 <template>
-  <div>Login index</div>
+  <div class="login-container">
+    <el-form
+      size="medium"
+    >
+      <img
+        src="src/assets/login-logo.png"
+        alt="登录logo"
+        style="height: 80px"
+      >
+      <el-form-item>
+        <el-input placeholder="请输入用户名">
+          <template #prefix>
+            <el-icon>
+              <user />
+            </el-icon>
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input placeholder="请输入密码">
+          <template #prefix>
+            <el-icon>
+              <lock />
+            </el-icon>
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item>
+        <div style="display: flex; justify-content: space-between; width: 100%">
+          <el-input placeholder="请输入验证码">
+            <template #prefix>
+              <el-icon>
+                <key />
+              </el-icon>
+            </template>
+          </el-input>
+          <img
+            src="https://shop.fed.lagounews.com/api/admin/captcha_pro?1651148848000"
+            style="height: 31px;"
+          >
+        </div>
+      </el-form-item>
+      <el-button
+        style="width: 100%"
+        type="primary"
+        size="large"
+      >
+        登录
+      </el-button>
+    </el-form>
+  </div>
 </template>
+<script lang="ts" setup>
 
-<script lang='ts' setup>
-
+import { Key } from '@element-plus/icons-vue'
 </script>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
+.login-container {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #2c3e50;
+}
 
+.el-form {
+  background-color: white;
+  padding: 40px;
+  border-radius: 8px;
+}
 </style>
